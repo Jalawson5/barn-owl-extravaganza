75365d9c6d012fe77653c5ee0a2abefbcd30403d
Justin_Lawson <ultradinosaur123@gmail.com>
2021-05-07 17:03:18 -0400

    Added a Dialogue Object
    Added an object that stores dialogue for one conversation.

---
+++ Metroidvania/Metroidvania/Assets/ScriptableObjects/DialogueEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
+public class DialogueEntry : ScriptableObject
+{
+    public int id;
+    public string[] texts;
+}
